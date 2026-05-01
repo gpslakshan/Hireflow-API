cf5ca487586e142356577439d903689f86ea5cb6
Sachin <sachinlakshan04@gmail.com>
2026-05-01 19:56:08 +0530

    define service interfaces

---
+++ internal/service/interfaces.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"github.com/google/uuid"
+	"github.com/gpslakshan/hireflow/internal/domain/dto"
+	"github.com/gpslakshan/hireflow/internal/domain/entity"
+)
+
+type AuthServiceInterface interface {
+	Register(req dto.RegisterRequest) (entity.User, error)
+	Login(req dto.LoginRequest) (string, entity.User, error)
+	GetByID(id string) (*entity.User, error)
+}
+
+type CompanyServiceInterface interface {
+	Create(req dto.CreateCompanyRequest) (entity.Company, error)
+	GetAll() ([]entity.Company, error)
+	GetByID(id uuid.UUID) (entity.Company, error)
+	Update(id uuid.UUID, req dto.UpdateCompanyRequest) (entity.Company, error)
+	Delete(id uuid.UUID) error
+}
+
+type JobServiceInterface interface {
+	Create(companyID uuid.UUID, posterID uuid.UUID, req dto.CreateJobRequest) (entity.Job, error)
+	GetAll() ([]entity.Job, error)
+	GetByID(id uuid.UUID) (entity.Job, error)
+	Update(id uuid.UUID, recruiterID uuid.UUID, req dto.UpdateJobRequest) (entity.Job, error)
+	Close(id uuid.UUID, recruiterID uuid.UUID) (entity.Job, error)
+	Delete(id uuid.UUID, recruiterID uuid.UUID) error
+}
+
+type ApplicationServiceInterface interface {
+	Apply(jobID uuid.UUID, candidateID uuid.UUID, req dto.ApplyJobRequest) (entity.Application, error)
+	GetMyApplications(candidateID uuid.UUID) ([]entity.Application, error)
+	GetByJob(jobID uuid.UUID, recruiterID uuid.UUID) ([]entity.Application, error)
+	GetByID(id uuid.UUID, userID uuid.UUID, role string) (entity.Application, error)
+	UpdateStatus(id uuid.UUID, recruiterID uuid.UUID, req dto.UpdateApplicationStatusRequest) (entity.Application, error)
+	Withdraw(id uuid.UUID, candidateID uuid.UUID) error
+}
